ad0d2691f7d6f0c10ea3af96e1105a00809c11ea
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2020-02-13 01:34:30 +0530

    misc fixes, add full buffering
    also lock memory pages in RAM to prevent swapping out to disk

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 
 #include "driver.h"
+#include "log.h"
+
+extern char log_buf[];
 
 int main(int argc, char *argv[]) {
 	int r;
+
+	r = mlockall(MCL_FUTURE);
+	if (r < 0) {
+		log_warn("Failed to lock pages in memory, ignoring: %m");
+	}
+
+	r = setvbuf(stderr, log_buf, _IOFBF, LOG_BUF_SIZE);
+	if (!r) {
+		log_warn("Failed to set full buffering, ignoring: %m");
+	}
+
 	if (argc > 1) {
 		log_error("Program takes no arguments");
 		return EXIT_FAILURE;
